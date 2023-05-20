f77c41f144eb97c6f511a7be7d3a678ef1585167
dreamer8313 <lshenae@connect.ust.hk>
2023-05-20 23:09:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'SHEN-Lifeng-8de32c1c8bb84fd7bbfaf6a09a299165',
+  rootNotionPageId: '4937ffa8a8454f72bfebef787ca9b388',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Hello World',
-  domain: 'https://lshenae.notion.site/SHEN-Lifeng-8de32c1c8bb84fd7bbfaf6a09a299165',
+  domain: 'https://lshenae.notion.site/a9f070835d7d446c93a3ff98102cefb1?v=4937ffa8a8454f72bfebef787ca9b388',
   author: 'Researcher Lifeng',
 
   // open graph metadata (optional)
@@ -55,7 +55,7 @@ export default siteConfig({
      navigationLinks: [
      {
        title: 'HOME',
-       pageId: 'SHEN-Lifeng-8de32c1c8bb84fd7bbfaf6a09a299165'
+       pageId: '4937ffa8a8454f72bfebef787ca9b388'
      }
   //   {
   //     title: 'READ',
