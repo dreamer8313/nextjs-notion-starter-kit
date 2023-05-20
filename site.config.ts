bf7e94a42985efccdf951f9f433198217440e2d9
dreamer8313 <lshenae@connect.ust.hk>
2023-05-20 22:49:52 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -28,7 +28,7 @@ export default siteConfig({
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
-  defaultPageCoverPosition: 0.85,
+  defaultPageCoverPosition: 0.75,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
